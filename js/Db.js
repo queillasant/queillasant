8f760135384cba22ab503dd940a3bf6a4adf0c52
queillasant <55539446+queillasant@users.noreply.github.com>
2019-11-21 07:59:52 -0300

    Update Db.js

--- js/Db.js
+++ js/Db.js
@@ -1,4 +1,4 @@
-db.enablePersistence()
+Db.enablePersistence()
     .catch(function(err) {
         if (err.code == 'failed-precondition') {
             console.log('persistance failed');
@@ -7,7 +7,7 @@ db.enablePersistence()
         }
     });
 
-db.collection('produtos').onSnapshot(snapshot => {
+Db.collection('produtos').onSnapshot(snapshot => {
     snapshot.docChanges().forEach(change => {
         if (change.type === 'added') {
             renderRecipe(change.doc.data(), change.doc.id);
@@ -16,3 +16,27 @@ db.collection('produtos').onSnapshot(snapshot => {
         }
     });
 });
+
+// adicionar um novo produto
+const form = document.querySelector('form');
+form.addEventListener('submit', evt => {
+    evt.preventDefault();
+
+    const produtos = {
+        nome: form.produtoTitulo.value,
+        descricao: form.produtoDescricao.value,
+        link: form.produtoLink.value,
+        endereco_imagem: form.produtoArquivo.value
+    };
+
+    Db.collection('produtos').add(produto)
+        .catch(err => console.log(err));
+
+    //reseta o formulario
+    form.produtoTitulo.value = '';
+    form.produtoDescricao.value = '';
+    form.produtoLink.value = '';
+    form.produtoArquivo.value = '';
+
+});
+
